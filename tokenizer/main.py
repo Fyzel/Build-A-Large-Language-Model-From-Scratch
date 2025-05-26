c65f0bd0d69f2434240f604c8fcb1d41b10b1450
Fyzel <Fyzel@users.noreply.github.com>
2025-05-25 19:31:04 -0600

    Add SimpleTokenizerV1 class for text tokenization and vocabulary management

--- tokenizer/main.py
+++ tokenizer/main.py
@@ -41,3 +41,18 @@ if __name__ == "__main__":
 
     print("\nFirst 30 tokens:")
     print(preprocessed[:30])
+
+    print("\n----------------------------------------")
+    print("Create the sorted vocabulary")
+    all_words = sorted(set(preprocessed))
+    vocab_size = len(all_words)
+    print("\nVocabulary size:", vocab_size)
+
+    print("\nFirst 50 tokens in the vocabulary:")
+    vocab = {token: integer for integer, token in enumerate(all_words)}
+    for i, item in enumerate(vocab.items()):
+        print(item)
+
+        # Limit the output to the first 50 items
+        if i >= 50:
+            break
